0914e2a3c7870d8cecb3caef685f8e9585b23a38
Tobias Andersson <tobias.ande@gmail.com>
2021-09-23 23:20:41 +0200

    fix lint

--- src/extension.ts
+++ src/extension.ts
@@ -7,28 +7,28 @@ import * as fs from 'fs';
 export function activate(context: vscode.ExtensionContext) {
 
 
-	let orange = vscode.window.createOutputChannel("Orange");
+	const orange = vscode.window.createOutputChannel("Orange");
 
 	// initialize a new parser instance
-	var parser = new phpparser.Engine(
+	const parser = new phpparser.Engine(
 
 		{
 			parser: {
-			  debug: false, 
-			  locations: false,
-			  extractDoc: true,
-			  suppressErrors: false
+				debug: false,
+				locations: false,
+				extractDoc: true,
+				suppressErrors: false
 			},
 			ast: {
-			  withPositions: false,
-			  withSource: false
+				withPositions: false,
+				withSource: false
 			},
 			lexer: {
-			  all_tokens: false,
-			  comment_tokens: false,
-			  mode_eval: false,
-			  asp_tags: false,
-			  short_tags: false
+				all_tokens: false,
+				comment_tokens: false,
+				mode_eval: false,
+				asp_tags: false,
+				short_tags: false
 			}
 		}
 
@@ -45,9 +45,9 @@ export function activate(context: vscode.ExtensionContext) {
 		return readData('' + data);
 	});
 
-	const readData = function(data: string) {
+	const readData = function (data: string) {
 
-		fs.writeFile('DefaultSettings.php', data,  function(err) {
+		fs.writeFile('DefaultSettings.php', data, function (err) {
 			if (err) {
 				return orange.appendLine("failed to write file!");
 			}
@@ -56,24 +56,24 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const defaultSettings = parser.parseCode(data, 'DefaultSettings.php');
 		orange.appendLine(defaultSettings.children.length.toString());
-		let variables: string[] = [];
-		let comments: string[] = [];
-		let values : string[] = [];
-		let types : string[] = [];
+		const variables: string[] = [];
+		const comments: string[] = [];
+		const values: string[] = [];
+		const types: string[] = [];
 
-		for(var i = 0 ; i < defaultSettings.children.length; i++ ) {
-			const element:phpparser.Node = defaultSettings.children[i];
+		for (let i = 0; i < defaultSettings.children.length; i++) {
+			const element: phpparser.Node = defaultSettings.children[i];
 			const json = JSON.parse(JSON.stringify(element));
-			if( ( json['kind'] === 'expressionstatement' ) ) {
-				if(json['expression']['kind'] === 'assign') {
-					let variableName = json['expression']['left']['name']
-					if( !variableName ){
+			if ((json['kind'] === 'expressionstatement')) {
+				if (json['expression']['kind'] === 'assign') {
+					let variableName = json['expression']['left']['name'];
+					if (!variableName) {
 						let what = json['expression']['left']['what'];
-						while( !what['name'] && what['what']) {
+						while (!what['name'] && what['what']) {
 							what = what['what'];
 						}
 
-						if( !what['name'] ){
+						if (!what['name']) {
 							orange.appendLine("missing variable name: " + JSON.stringify(json['expression']['left']));
 							continue;
 						}
@@ -81,95 +81,97 @@ export function activate(context: vscode.ExtensionContext) {
 						variableName = what['name'];
 					}
 
-					if(  variables.includes(variableName) ) {
+					if (variables.includes(variableName)) {
 						continue;
 					}
-					
+
 					variables.push(variableName);
 					let value = "unknown";
 
 					const kind = json['expression']['right']['kind'];
 					const rawValue = json['expression']['right']['raw'];
 
-					if( kind === 'new' ) {
+					if (kind === 'new') {
 						value = 'new ' + json['expression']['right']['what']['name'];
 					} else if (kind === 'name') {
-						value = json['expression']['right']['name']
+						value = json['expression']['right']['name'];
 					} else if (rawValue) {
 						value = rawValue;
 					}
 
-					if( kind ) {
+					if (kind) {
 						types.push(kind);
 					} else {
-						types.push('unkown')
+						types.push('unkown');
 					}
 
 					let leadingComment = "";
 
-					if(json['leadingComments']) {
-						for(var i = 0; i < json['leadingComments'].length; i++){
-							try{
-								let comment: String = json['leadingComments'][i]['value'];
+					if (json['leadingComments']) {
+						for (let i = 0; i < json['leadingComments'].length; i++) {
+							try {
+								const comment: string = json['leadingComments'][i]['value'];
 
-								if(!comment){
+								if (!comment) {
 									continue;
 								}
-	
-								if( !comment.includes("@endcond") && !comment.includes("@phan") ) {
+
+								if (!comment.includes("@endcond") && !comment.includes("@phan")) {
 									leadingComment += comment;
-								}	
-							}catch(e){
+								}
+							} catch (e) {
 								orange.appendLine("failed parsing comment: " + JSON.stringify(json));
 							}
 						}
 					}
 					comments.push(leadingComment);
 					values.push(value);
-					
+
 				}
 
 			}
 
 
-		};
+		}
 
 		orange.appendLine("added " + variables.length + " variables.");
 
-		const statements : vscode.CompletionItem[] = [];
-				
-				for(var i = 0 ; i < variables.length; i++ ){
-					const completionSuggestion = "$" + variables[i];
-					const manualLink = "// https://www.mediawiki.org/wiki/Manual:\\\$" + variables[i];
-					const insertText = comments[i] + "\n" + manualLink + "\n\\\$" + variables[i] + " = " + values[i] + ';';
+		const statements: vscode.CompletionItem[] = [];
 
-					const snippetCompletion = new vscode.CompletionItem(completionSuggestion);
-					snippetCompletion.insertText = new vscode.SnippetString(insertText);
-					snippetCompletion.documentation = new vscode.MarkdownString(comments[i]);
-					snippetCompletion.detail = types[i];
+		for (let i = 0; i < variables.length; i++) {
+			const completionSuggestion = "$" + variables[i];
+			// eslint-disable-next-line no-useless-escape
+			const manualLink = "// https://www.mediawiki.org/wiki/Manual:\\\$" + variables[i];
+			// eslint-disable-next-line no-useless-escape
+			const insertText = comments[i] + "\n" + manualLink + "\n\\\$" + variables[i] + " = " + values[i] + ';';
 
-					statements.push(snippetCompletion);
-				};
+			const snippetCompletion = new vscode.CompletionItem(completionSuggestion);
+			snippetCompletion.insertText = new vscode.SnippetString(insertText);
+			snippetCompletion.documentation = new vscode.MarkdownString(comments[i]);
+			snippetCompletion.detail = types[i];
+
+			statements.push(snippetCompletion);
+		}
 
 		const provider1 = vscode.languages.registerCompletionItemProvider('php', {
-				provideCompletionItems(
-					document: vscode.TextDocument, 
-					position: vscode.Position, 
-					token: vscode.CancellationToken, 
-					context: vscode.CompletionContext
-				) {
+			provideCompletionItems(
+				document: vscode.TextDocument,
+				position: vscode.Position,
+				token: vscode.CancellationToken,
+				context: vscode.CompletionContext
+			) {
 				return statements;
 			}
 		});
 		context.subscriptions.push(provider1);
-	
-	}
-	const readFromSource = function() {
+
+	};
+	const readFromSource = function () {
 		//Write to output.
 		axios.get(
 			'https://raw.githubusercontent.com/wikimedia/mediawiki/master/includes/DefaultSettings.php',
-		).then( (resp) => {
+		).then((resp) => {
 			readData(resp.data);
 		});
-	}
+	};
 }
